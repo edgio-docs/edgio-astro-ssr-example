3cc9afbea22c1dc7676177cd9eda797566e69004
Rishi Raj Jain <46300090+rishi-raj-jain@users.noreply.github.com>
2022-08-19 01:03:01 +0530

    Abstract Cache Handler

--- routes.ts
+++ routes.ts
@@ -1,27 +1,17 @@
 import { Router } from '@layer0/core/router'
 
+const ONE_DAY_CACHE = ({ cache }) => {
+  cache({
+    edge: {
+      maxAgeSeconds: 24 * 60 * 60,
+    },
+  })
+}
+
 export default new Router()
-  .match('/', ({ cache }) => {
-    cache({
-        edge: {
-           maxAgeSeconds: 24 * 60 * 60
-        }
-    })
-  })
-  .match('/assets/:path*', ({ cache }) => {
-    cache({
-        edge: {
-           maxAgeSeconds: 24 * 60 * 60
-        }
-    })
-  })
-  .match('/images/:path*', ({ cache }) => {
-    cache({
-        edge: {
-           maxAgeSeconds: 24 * 60 * 60
-        }
-    })
-  })
+  .match('/', ONE_DAY_CACHE)
+  .match('/assets/:path*', ONE_DAY_CACHE)
+  .match('/images/:path*', ONE_DAY_CACHE)
   .fallback(({ renderWithApp }) => {
     renderWithApp()
   })
