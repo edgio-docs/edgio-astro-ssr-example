e22ceb0ae08a5e9d827ca3135bb926239105828f
Rishi Raj Jain <46300090+rishi-raj-jain@users.noreply.github.com>
2022-08-19 01:47:12 +0530

    Build SW in dev mode

--- layer0/dev.js
+++ layer0/dev.js
@@ -1,6 +1,20 @@
+const { buildSync } = require('esbuild')
 const { createDevServer } = require('@layer0/core/dev')
 
+const appDir = process.cwd()
+
 module.exports = function () {
+  buildSync({
+    entryPoints: [`${appDir}/sw/service-worker.js`],
+    outfile: `${appDir}/dist/service-worker.js`,
+    minify: true,
+    bundle: true,
+    define: {
+      'process.env.NODE_ENV': '"production"',
+      'process.env.LAYER0_PREFETCH_HEADER_VALUE': '"1"',
+      'process.env.LAYER0_PREFETCH_CACHE_NAME': '"prefetch"',
+    },
+  })
   return createDevServer({
     label: '[Astro SSR]',
     command: (port) => `npm run dev -- --port ${port}`,
