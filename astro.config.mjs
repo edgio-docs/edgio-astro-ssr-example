a090cff10ce23180c8cfd936df026d5a2739c9d1
Rishi Raj Jain <rishi18304@iiitd.ac.in>
2022-10-22 21:00:10 +0530

    Upgrade to edgio@5

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,14 @@
-import node from '@astrojs/node'
 import react from '@astrojs/react'
 import tailwind from '@astrojs/tailwind'
 import { defineConfig } from 'astro/config'
 
+import node from '@astrojs/node'
+
+// https://astro.build/config
 export default defineConfig({
-  output: 'server',
-  adapter: node(),
   integrations: [react(), tailwind()],
+  output: 'server',
+  adapter: node({
+    mode: 'standalone',
+  }),
 })
